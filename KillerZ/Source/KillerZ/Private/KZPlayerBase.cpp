70b669a8cd72e3ef9b266e7db3a19a8d10d9f4a0
FFUlgen <fevziemirulgen@gmail.com>
2023-12-24 00:19:27 +0300

    Final Project Setup

---
+++ KillerZ/Source/KillerZ/Private/KZPlayerBase.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KZPlayerBase.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "InputAction.h"
+
+AKZPlayerBase::AKZPlayerBase() 
+{
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+
+	SpringArm->SetupAttachment(GetMesh());
+	Camera->SetupAttachment(SpringArm);
+}
+
+void AKZPlayerBase::BeginPlay()
+{
+
+}
+
+void AKZPlayerBase::EnhancedInputMove(const FInputActionValue& Value)
+{
+}
+
+void AKZPlayerBase::Tick(float DeltaTime)
+{
+
+}
+
+void AKZPlayerBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
