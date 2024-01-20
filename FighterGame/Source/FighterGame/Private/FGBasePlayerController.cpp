27421e0bd6f934bb9872a29b39f3ac1fe3f74d39
FFUlgen <fevziemirulgen@gmail.com>
2024-01-20 11:42:26 +0300

    Deleted Curropted Files

--- FighterGame/Source/FighterGame/Private/FGBasePlayerController.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "FGBasePlayerController.h"
-
-void AFGBasePlayerController::BeginPlay()
-{
-	Super::BeginPlay();
-}
--- FighterGame/Source/FighterGame/Private/Game/FGGameModeBase.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Game/FGGameModeBase.h"
-#include "FGCharacterBase.h"
-
-AFGGameModeBase::AFGGameModeBase()
-{
-}
